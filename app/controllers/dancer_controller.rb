50030b73b939031d68dec368d11db337286f111d
galantlex <Alexis.Galantino@tufts.edu>
2016-12-25 23:49:07 -0500

    about to delete database

--- app/controllers/dancer_controller.rb
+++
@@ -1,9 +0,0 @@
-class DancerController < ApplicationController
-  def index
-    @dancers = Dancer.all
-  end
-  def show
-    @dancer = Dancer.find(params[:id])
-    @seeks = @dancer.seeks.all
-  end
-end
--- app/controllers/seek_controller.rb
+++ app/controllers/seek_controller.rb
@@ -1,2 +1,29 @@
 class SeekController < ApplicationController
+  def new
+    @seek = Seek.new
+    @roles = ['Follower', 'Leader']
+    @styles = ['Rhythm', 'Latin', 'Smooth', 'Standard']
+    @levels = ['Newcomer', 'Bronze', 'Silver', 'Gold', 'Novice', 'Prechamp',
+                   'Champ']
+  end
+  def create
+	@seek = Seek.new(seek_params)
+	@seek.dancer_id = current_user.id
+        @roles = ['Follower', 'Leader']
+        @styles = ['Rhythm', 'Latin', 'Smooth', 'Standard']
+        @levels = ['Newcomer', 'Bronze', 'Silver', 'Gold', 'Novice', 'Prechamp',
+                   'Champ']
+        if @seek.save
+      	  redirect_to root
+    	else
+          redirect_to root
+    	end
+  end
+  
+  private
+  
+  def seek_params
+    params.require(:seek).permit(:dancer_id, :comp_id, :role, :style,
+                                 :level)
+  end
 end
